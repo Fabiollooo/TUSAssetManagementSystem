28d946bf3cea7b69d7b4fbd69407e30da8913e32
armanrhossain <167314696+armanrhossain@users.noreply.github.com>
2025-11-05 18:04:46 +0000

    Add project files.

---
+++ BusinessLayer/IModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BusinessEntities;
+using System.Collections.Generic;
+namespace BusinessLayer
+{
+    public interface IModel
+    {
+        bool addNewUser(string name, string password, string userType);
+        BusinessEntities.IUser CurrentUser { get; set; }
+        DataAccessLayer.IDataLayer DataLayer { get; set; }
+        bool deleteUser(IUser user);
+        bool editUser(BusinessEntities.IUser user);
+        string getUserTypeForCurrentuser();
+        bool login(string name, string password);
+        List<IProductLine> ProductLineList { get; set; }
+        void tearDown();
+        List<IUser> UserList { get; }
+        List<IOrder> OrderList { get; set; }
+
+        void populateOrders();
+        bool updateProductLine(IProductLine productLine);
+    }
+}
