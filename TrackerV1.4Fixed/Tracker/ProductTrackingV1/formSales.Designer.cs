8595e6dedbc881598c59047f779ad1bd1f565ea6
Fabian00295775 <k00295775@student.tus.ie>
2026-04-17 14:50:26 +0100

    Version 01

---
+++ TrackerV1.4Fixed/Tracker/ProductTrackingV1/formSales.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ProductTracking
+{
+    partial class formSales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewRooms = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRooms)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExit.Location = new System.Drawing.Point(401, 440);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 48);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Available Rooms";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridViewRooms
+            // 
+            this.dataGridViewRooms.AllowUserToAddRows = false;
+            this.dataGridViewRooms.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewRooms.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRooms.Location = new System.Drawing.Point(45, 92);
+            this.dataGridViewRooms.Name = "dataGridViewRooms";
+            this.dataGridViewRooms.ReadOnly = true;
+            this.dataGridViewRooms.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewRooms.TabIndex = 2;
+            this.dataGridViewRooms.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewRooms_CellContentClick);
+            // 
+            // formSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProductTracking.Properties.Resources.Blue_hills3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.ControlBox = false;
+            this.Controls.Add(this.dataGridViewRooms);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExit);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "formSales";
+            this.Text = "Sales";
+            this.Load += new System.EventHandler(this.formSales_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRooms)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewRooms;
+    }
+}
---
+++ TrackerV1.4Fixed/Tracker/ProductTrackingV1/formSales.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BusinessLayer;
+
+namespace ProductTracking
+{
+    public partial class formSales : Form
+    {
+        #region Instance Attributes
+        private formContainer fc;
+        private IModel Model;
+        #endregion
+        #region Constructors
+        public formSales(formContainer parent, IModel Model)
+        {
+            InitializeComponent();
+            MdiParent = parent;
+            fc = parent;
+            this.Model = Model;
+        }
+        #endregion
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            //if (Model.Con.State.ToString() == "Open")
+            //    Model.Con.Close();
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewRooms_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void formSales_Load(object sender, EventArgs e)
+        {
+            //Available Rooms - 
+            DataTable rooms = new DataTable();
+
+            rooms.Columns.Add("RoomNumber");
+            rooms.Columns.Add("Capacity");
+            rooms.Columns.Add("Available");
+
+            rooms.Rows.Add("R101", "4", "Y");
+            rooms.Rows.Add("R102", "6", "N");
+            rooms.Rows.Add("R103", "2", "Y");
+            rooms.Rows.Add("R104", "8", "N");
+            rooms.Rows.Add("R105", "3", "Y");
+
+            dataGridViewRooms.DataSource = rooms;
+        }
+    }
+}
